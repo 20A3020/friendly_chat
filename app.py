bf9295c4f24be14fbb0f474d52be927a9f24cf5b
20A3020 <139091135+20A3020@users.noreply.github.com>
2023-11-06 10:30:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,21 +7,21 @@ openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+    st.session_state["messages"] = [
+        {"role": "system", "content": "あなたは文章校正専門のAIです。"},
+            {"role": "user", "content": "次に入力される文章に対して、『誤字,脱字の訂正』『適切な改行,読点,句読点の挿入』『主張を要約した1文を追加』の3項目を行ってください。"}
+        ]
 
 # チャットボットとやりとりする関数
 def communicate():
     messages = st.session_state["messages"]
 
-    user_message = {"role": "assistant", "content": st.session_state["user_input"]}
+    user_message = {"role": "user", "content": st.session_state["user_input"]}
     messages.append(user_message)
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "あなたは文章校正専門のAIです。"},
-            {"role": "user", "content": "次に入力される文章に対して、『誤字,脱字の訂正』『適切な改行,読点,句読点の挿入』『主張を要約した1文を追加』の3項目を行ってください。"}
-        ]
+        messages=messages
     )
 
     bot_message = response["choices"][0]["message"]
@@ -32,7 +32,7 @@ def communicate():
 
 # ユーザーインターフェイスの構築
 st.title("校正はお任せ！")
-st.write("ChatGPT APIを使ってあなたの卒論を校正します。")
+st.write("ChatGPT APIを使ってあなたの文章を校正します。")
 
 user_input = st.text_input("校正したい文章を入力してください。", key="user_input", on_change=communicate)
 

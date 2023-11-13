fbfb78e474efe783d7cbb505426b059692b52281
20A3020 <139091135+20A3020@users.noreply.github.com>
2023-11-13 10:04:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import openai
 
@@ -9,7 +8,7 @@ openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
         {"role": "system", "content": "あなたは文章校正専門のAIです。"},
-        {"role": "user", "content": "次に入力される文章に対して、『誤字,脱字の訂正』『適切な改行,読点,句読点の挿入』『主張を要約した1文を追加』の3項目を行ってください。"}
+            {"role": "user", "content": "次に入力される文章に対して、『誤字,脱字の訂正』『適切な改行,読点,句読点の挿入』『主張を要約した1文を追加』の3項目を行ってください。"}
         ]
 
 # チャットボットとやりとりする関数
@@ -32,7 +31,7 @@ def communicate():
 
 # ユーザーインターフェイスの構築
 st.title("校正はお任せ！")
-st.write("ChatGPT APIを使ってあなたの文章を校正します。")
+st.write("ChatGPT APIを使ってあなたの卒論を校正します。")
 
 user_input = st.text_input("校正したい文章を入力してください。", key="user_input", on_change=communicate)
 

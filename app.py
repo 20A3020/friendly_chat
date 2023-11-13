d20585424a837c627c08eccac462c55be3df6a14
20A3020 <139091135+20A3020@users.noreply.github.com>
2023-11-13 11:02:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
-import openai
+from openai import OpenAI
 
+client = OpenAI(api_key=st.secrets.OpenAIAPI.openai_api_key)
 # Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
-openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
@@ -10,34 +10,27 @@ if "messages" not in st.session_state:
         {"role": "system", "content": "あなたは文章校正専門のAIです。"},
         {"role": "user", "content": "次に入力される文章に対して、『誤字,脱字の訂正』『適切な改行,読点,句読点の挿入』『主張を要約した1文を追加』の3項目を行ってください。"}
         ]
-
+    
 # チャットボットとやりとりする関数
 def communicate():
     messages = st.session_state["messages"]
-
     user_message = {"role": "user", "content": st.session_state["user_input"]}
     messages.append(user_message)
-
-    response = openai.ChatCompletion.create(
-        model="gpt-4",
-        messages=messages
-    )
-
+    response = client.chat.completions.create(model="gpt-4",
+        messages=messages)
+    
     bot_message = response["choices"][0]["messages"]
     messages.append(bot_message)
-
+    
     st.session_state["user_input"] = ""  # 入力欄を消去
-
-
+    
 # ユーザーインターフェイスの構築
 st.title("校正はお任せ！")
 st.write("ChatGPT APIを使ってあなたの卒論を校正します。")
-
 user_input = st.text_input("校正したい文章を入力してください。", key="user_input", on_change=communicate)
 
 if st.session_state["messages"]:
     messages = st.session_state["messages"]
-
     for message in reversed(messages[1:]):  # 直近のメッセージを上に
         speaker = "👦あなたのテキスト"
         if message["role"]=="assistant":

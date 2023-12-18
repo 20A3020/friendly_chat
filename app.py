5d502ccf5f1901d7d90b074f77348f7d49f824ba
20A3020 <139091135+20A3020@users.noreply.github.com>
2023-12-18 12:12:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -26,11 +26,11 @@ if "messages" not in st.session_state:
 def communicate():
     messages = st.session_state["messages"]
 
-    if select_prompt == '友人向けメッセージ':
-        messages = [
-            {"role": "system", "content": "あなたは優秀な文章校正アシスタントAIです。"},
-            {"role": "user", "content": "友人に向けたメッセージを書きます。『誤字・脱字を訂正する』『親しく,砕けた口調にする』『要点は繰り返して強調する』という3つの条件に従って校正してください。"},
-            ]
+    #if select_prompt == '友人向けメッセージ':
+        #messages = [
+            #{"role": "system", "content": "あなたは優秀な文章校正アシスタントAIです。"},
+            #{"role": "user", "content": "友人に向けたメッセージを書きます。『誤字・脱字を訂正する』『親しく,砕けた口調にする』『要点は繰り返して強調する』という3つの条件に従って校正してください。"},
+            #]
     #elif select_prompt == 'ビジネスメール':
         #st.session_state["messages"] = [
             #{"role": "system", "content": "あなたは優秀な文章校正アシスタントAIです。"},

710b5c05ed1b8c2d462be67eedaf9cadb4bd4ca9
20A3020 <139091135+20A3020@users.noreply.github.com>
2023-12-18 11:57:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ if "messages" not in st.session_state:
 if select_prompt == '友人向けメッセージ':
     st.session_state["messages"] = [
         {"role": "system", "content": "あなたは優秀な文章校正アシスタントAIです。"},
-        {"role": "user", "content": "下記の文章は友人に向けたものです。『誤字・脱字を訂正する』『親しく,砕けた口調にする』『要点は繰り返して強調する』という3つの条件に従って校正してください。"}
+        {"role": "system", "content": "下記の文章は友人に向けたものです。『誤字・脱字を訂正する』『親しく,砕けた口調にする』『要点は繰り返して強調する』という3つの条件に従って校正してください。"}
         ]
 elif select_prompt == 'ビジネスメール':
     st.session_state["messages"] = [

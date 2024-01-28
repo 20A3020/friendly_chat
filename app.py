8e010924be8496b25e12d2bcf71567b6f2eb67ce
20A3020 <139091135+20A3020@users.noreply.github.com>
2024-01-28 23:35:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -57,6 +57,7 @@ def communicate():
 st.title("文章校正アシスタント")
 st.write("「その文章、誤って誰かを傷つけない？」「隠れたミス、見逃してない？」")
 st.write("文章に潜む間違い、お直しします。")
+st.write("")
 
 user_input = st.text_input("↓ここに校正したい文章を入力してください。", key="user_input", on_change=communicate)
 

a0f841323df202d791a95023704969e8e9ee2012
20A3020 <139091135+20A3020@users.noreply.github.com>
2024-01-29 04:40:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,11 @@ import openai
 # Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
-#select_prompt = st.sidebar.selectbox('誰に宛てたメッセージですか？', ['友人・知人', '先生', '上司', '論文など'])
+select_prompt = st.sidebar.selectbox('誰に向けた文章ですか?', ['友人', '先生'])
 #answer_volume = st.sidebar.slider('校正結果の出力数を決めてください。', 1, 3, 1)校正結果は" + str(answer_volume) + "個出力してください。
 
 # st.session_stateを使いメッセージのやりとりを保存    
-#if select_prompt == '友人・知人':
-if "messages" not in st.session_state:
+if select_prompt == '友人・知人':
     st.session_state["messages"] = [
         {"role": "system", "content": "あなたは優秀な文章校正アシスタントAIです。"},
         {"role": "user", "content": "友人へ宛てたメッセージを校正してください。尚、『誤字・脱字の訂正』『曖昧な表現の訂正』『要点の強調』という3つの条件を遵守してください。"}

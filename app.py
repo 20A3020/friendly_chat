830b86cf628704f51cd6a93f031d4e5273e6d56b
20A3020 <139091135+20A3020@users.noreply.github.com>
2024-02-01 11:21:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def communicate():
         #temperature = num/10
     )
 
-    bot_message = response["choices"][0]["message"]
+    bot_message = response["choices"][0]["messages"]
     messages.append(bot_message)
 
     st.session_state["user_input"] = ""  # 入力欄を消去

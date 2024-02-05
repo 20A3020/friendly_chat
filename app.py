897a880fec73198ce3a563ceaaf774e4d0522036
20A3020 <139091135+20A3020@users.noreply.github.com>
2024-02-05 13:49:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,25 +5,25 @@ import openai
 # Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
-sp = st.sidebar.selectbox('誰に向けた文章ですか?', ['友人', '目上の人', '論文など'])
+st.session_state["pro"] = st.sidebar.selectbox('誰に向けた文章ですか?', ['友人', '目上の人', '論文など'])
 ans = st.sidebar.slider('校正結果の出力数を決めてください。', 1, 3, 1)
 st.session_state["num"] = st.sidebar.slider('出力される校正結果の揺らぎを設定してください。(小さいほど回答が固定されます)', 0.0, 1.0, 0.7)
 
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
-    if sp == '友人':
+    if "pro" == '友人':
         st.session_state["messages"] = [
           {"role": "system", "content": "あなたは優秀な文章校正アシスタントAIです。"},
           {"role": "user", "content": "友人へ宛てたメッセージを校正してください。尚、『誤字・脱字の訂正』『曖昧な表現の訂正』『要点の強調』という3つの条件を遵守してください。"},
           {"role": "user", "content": "校正結果は" + str(ans) + "個出力してください。"},
           ]
-    elif sp == '目上の人':
+    elif "pro" == '目上の人':
         st.session_state["messages"] = [
           {"role": "system", "content": "あなたは優秀な文章校正アシスタントAIです。"},
           {"role": "user", "content": "先生へ宛てたメッセージを校正してください。尚、『礼節を弁えた,簡潔な文章にする』『誤字・脱字の訂正』『曖昧な表現の訂正』『要点の強調』という4つの条件を遵守してください。"},
           {"role": "user", "content": "校正結果は" + str(ans) + "個出力してください。"},
           ]
-    elif sp == '論文など':
+    elif "pro" == '論文など':
         st.session_state["messages"] = [
           {"role": "system", "content": "あなたは優秀な文章校正アシスタントAIです。"},
           {"role": "user", "content": "論文を校正してください。尚、『誤字・脱字の訂正』『曖昧な表現の訂正』『要点の強調』『表現・表記方法の統一』『だ・である調の文体にする』『整合性の取れない点を指摘する』『論文として適した文でない点を指摘する』という7つの条件を遵守してください。"},

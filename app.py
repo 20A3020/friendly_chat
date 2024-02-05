0ce52a9ecafad265e0713446d1a57766ad19bf51
20A3020 <139091135+20A3020@users.noreply.github.com>
2024-02-05 12:09:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ ans = st.sidebar.slider('校正結果の出力数を決めてください。', 1
 num = st.sidebar.slider('出力される校正結果の揺らぎを設定してください。(小さいほど回答が固定されます)', 0.0, 1.0, 0.7)
 
 st.session_state["select_prompt"] = sp
-#st.session_state["answer"] = ans
+st.session_state["answer"] = ans
 #st.session_state["number"] = num
 
 # st.session_stateを使いメッセージのやりとりを保存
@@ -18,9 +18,9 @@ if st.session_state["select_prompt"] == '友人' and "messages" not in st.sessio
     st.session_state["messages"] = [
         {"role": "system", "content": "あなたは優秀な文章校正アシスタントAIです。"},
         {"role": "user", "content": "友人へ宛てたメッセージを校正してください。尚、『誤字・脱字の訂正』『曖昧な表現の訂正』『要点の強調』という3つの条件を遵守してください。"},
-        {"role": "user", "content": "校正結果は" + str(ans) + "個出力してください。"},
+        {"role": "user", "content": "校正結果は" + str("answer") + "個出力してください。"},
         ]
-elif sp == '先生' and "messages" in st.session_state:
+elif st.session_state["select_prompt"] == '先生' and "messages" in st.session_state:
     st.session_state["messages"] = [
         {"role": "system", "content": "あなたは優秀な文章校正アシスタントAIです。"},
         {"role": "user", "content": "先生へ宛てたメッセージを校正してください。尚、『敬語を使う』『誤字・脱字の訂正』『曖昧な表現の訂正』『要点の強調』という4つの条件を遵守してください。"},
